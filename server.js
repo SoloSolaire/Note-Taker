6ded2287e23ebcb7e671aa742ab266a602983dd9
SoloSolaire <128697724+SoloSolaire@users.noreply.github.com>
2023-06-20 15:38:42 -0700

    debugged htmlrouter routing

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
 
-const htmlRouter = ('./routes/html');
+const htmlRouter = require('./routes/html');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
 app.use(htmlRouter);
 
-app.listen(PORT, () => console.log(`Listening for requests on port${PORT}!`));
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
