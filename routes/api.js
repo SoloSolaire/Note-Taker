0e177b0b3af07121df14b2fb411ba21c3aa50095
SoloSolaire <128697724+SoloSolaire@users.noreply.github.com>
2023-06-20 16:23:59 -0700

    api write works and writes to db.json now

--- routes/api.js
+++ routes/api.js
@@ -2,3 +2,24 @@ const api = require('express').Router();
 const fs = require('fs');
 let notes = require('../db/db.json');
 const { v4: uuidv4 } = require('uuid');
+
+api.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+api.post('/notes', (req, res) => {
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4()
+    };
+    notes.push(newNote);
+    res.json(newNote);
+
+    let notesString = JSON.stringify(notes);
+
+    fs.writeFile('./db/db.json', notesString, (err) => 
+    (err ? console.error(err) : console.log(`A new note has been added.`)))
+});
+
+module.exports = api;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const htmlRouter = require('./routes/html');
+const apiRouter = require('./routes/api')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,5 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(htmlRouter);
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
