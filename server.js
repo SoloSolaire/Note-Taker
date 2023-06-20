ed403bc0cd732ce5d7c86e4a307c204ec784ad2c
SoloSolaire <128697724+SoloSolaire@users.noreply.github.com>
2023-06-20 13:05:47 -0700

    added html routing

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
 
+const htmlRouter = ('./routes/html');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.static('public'));
+
+app.use(htmlRouter);
+
+app.listen(PORT, () => console.log(`Listening for requests on port${PORT}!`));
